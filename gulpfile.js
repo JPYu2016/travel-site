f19a34a2eb3fb989d81bb1ff32e4c269b00fb164
JPYu <niyujianping@hotmail.com>
2019-02-02 18:49:50 -0800

    Update changes on html & css til session 9

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
+
+
+/*
 var gulp = require('gulp'),
 watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested')
-cssImport = require('postcss-import');
+browserSync = require('browser-sync').create();
+*/
 
+/* no longer needed
 gulp.task('default', function() {
     console.log("Hooray - you created a Gulp task.")
 });
@@ -13,21 +16,4 @@ gulp.task('default', function() {
 gulp.task('html', function(){
     console.log("Imagine something useful being done to your HTML here.")
 });
-
-gulp.task('styles', function(){
-    return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-        .pipe(gulp.dest('./app/temp/styles'));
-//  console.log("Imagine Sass or PostCSS tasks running here.");
-});
-
-gulp.task('watch', function(){
-    watch('./app/index.html', function() {
-        gulp.start('html');
-    });
-
-    watch('./app/assets/styles/**/*.css', function() {
-        gulp.start('styles');
-    });
-
-});
+*/
