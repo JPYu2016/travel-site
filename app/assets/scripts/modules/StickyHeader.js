bc6363a1838bb73341fc47da2ed92ee0c103dc05
JPYu <niyujianping@hotmail.com>
2019-02-12 20:14:25 -0800

    Completed Lazyload adjustment

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -4,14 +4,28 @@ import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
     constructor() {
+        this.lazyImages = $(".lazyload");
         this.siteHeader = $(".site-header");
         this.headerTriggerElement = $(".large-hero__title");
         this.createHeaderWaypoint();
         this.pageSections = $(".page-section");
         this.headerLinks = $(".primary-nav a");
         this.createPageSectionWaypoints();
-        this.addSmoothScrolling();    
+        this.addSmoothScrolling();
+        this.refreshWaypoints();
     }
+    
+    refreshWaypoints() {
+        this.lazyImages.on('load', function() {
+            Waypoint.refreshAll();
+        });    
+    /*
+        this.lazyImages.load(function() {
+            Waypoint.refreshAll();
+        });
+    */
+    }
+    
 
     addSmoothScrolling() {
         this.headerLinks.smoothScroll();
